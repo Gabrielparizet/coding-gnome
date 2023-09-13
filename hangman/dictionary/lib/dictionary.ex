7870db0022ab7c662819d2f97de796a6f1e35dca
Gabrielparizet <parizetgabriel@gmail.com>
2023-09-13 12:00:22 +0200

    feat(hangman/dictionary): Added word_list.txt in assets and built random_world function in dictionary.ex

--- hangman/dictionary/lib/dictionary.ex
+++ hangman/dictionary/lib/dictionary.ex
@@ -1,5 +1,10 @@
 defmodule Dictionary do
-  def hello do
-    IO.puts "!!Hello world!!"
+  def word_list do
+    words = File.read!("assets/words.txt")
+    String.split(words, ~r/\n/, trim: true)
+  end
+
+  def random_word do
+    Enum.random(word_list())
   end
 end
