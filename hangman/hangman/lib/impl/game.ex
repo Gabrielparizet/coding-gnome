19123263e0c0ed3e94ab01ba334706831f09c8f8
Gabrielparizet <parizetgabriel@gmail.com>
2023-09-19 18:16:20 +0200

    feat(hangman/hangman) Implement make_move and tests

--- hangman/hangman/lib/impl/game.ex
+++ hangman/hangman/lib/impl/game.ex
@@ -16,6 +16,8 @@ defmodule Hangman.Impl.Game do
     used:       MapSet.new()
   )
 
+##############################################################################
+
   @spec new_game() :: t
   def new_game do
     new_game(Dictionary.random_word)
@@ -25,6 +27,24 @@ defmodule Hangman.Impl.Game do
   def new_game(word) do
     %__MODULE__{
       letters: word |> String.codepoints
+    }
+  end
+
+##############################################################################
+
+  @spec make_move(t, String.t()) :: { t, Type.tally }
+  def make_move(game = %{ game_state: state }, _guess)
+  when state in [:won, :lost] do
+    { game, tally(game) }
+  end
+
+
+  defp tally(game) do
+    %{
+      turns_left: game.turns_left,
+      game_state: game.game_state,
+      letters:    [],
+      used:       game.used |> MapSet.to_list |> Enum.sort
     }
   end
 
