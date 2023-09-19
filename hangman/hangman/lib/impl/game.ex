9ac6e26d0fdbe68df60a89ab896423fb9aaf75bf
Gabrielparizet <parizetgabriel@gmail.com>
2023-09-19 17:47:37 +0200

    feat(hangman/hangman) Refactoring types in type.ex

--- hangman/hangman/lib/impl/game.ex
+++ hangman/hangman/lib/impl/game.ex
@@ -1,23 +1,27 @@
 defmodule Hangman.Impl.Game do
 
-  @type t :: %Hangman.Impl.Game{
+  alias Hangman.Type
+
+  @type t :: %__MODULE__{
     turns_left: integer(),
-    game_state: Hangman.state,
-    letters: list(String.t()),
-    used: MapSet.t(String.t()),
+    game_state: Type.state,
+    letters:    list(String.t()),
+    used:       MapSet.t(String.t()),
   }
 
   defstruct(
     turns_left: 7,
     game_state: :initializing,
-    letters: [],
-    used: MapSet.new()
+    letters:    [],
+    used:       MapSet.new()
   )
 
+  @spec new_game() :: t
   def new_game do
     new_game(Dictionary.random_word)
   end
 
+  @spec new_game(String.t()) :: t
   def new_game(word) do
     %__MODULE__{
       letters: word |> String.codepoints
