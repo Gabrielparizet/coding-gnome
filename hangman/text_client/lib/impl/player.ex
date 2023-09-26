6771bc8a36dfe728796b0a3ecf7d4f8b3174210c
Gabrielparizet <parizetgabriel@gmail.com>
2023-09-26 17:11:18 +0200

    feat(hangman/text_client): Finish Up The Text Client

--- hangman/text_client/lib/impl/player.ex
+++ hangman/text_client/lib/impl/player.ex
@@ -26,11 +26,10 @@ defmodule TextClient.Impl.Player do
   end
 
   def interact({ game, tally }) do
-    IO.puts feedback_for(tally)
-    #display current word
-    # get next guess
-    # make move
-    # interact()
+    IO.puts(feedback_for(tally))
+    IO.puts(current_word(tally))
+    Hangman.make_move(game, get_guess())
+    |> interact()
   end
 
     # @type state :: :initializing | :good_guess | :bad_guess | :already_used
@@ -42,5 +41,20 @@ defmodule TextClient.Impl.Player do
   def feedback_for(%{ game_state: :bad_guess }),    do: "Sorry, that letter is not in the word"
   def feedback_for(%{ game_state: :already_used }), do: "You already used that letter"
 
+  def current_word(tally) do
+    [
+      "Word so far: ",
+      tally.letters |> Enum.join(" "),
+      "   turns left: ",
+      tally.turns_left |> to_string,
+      "   used so far ",
+      tally.used |> Enum.join(",")
+    ]
+  end
 
+  def get_guess() do
+    IO.gets("Next letter: ")
+    |> String.trim()
+    |> String.downcase()
+  end
 end
